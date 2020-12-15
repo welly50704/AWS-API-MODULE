320811b2f8f8c7544ed3559de233b019f48a7ed1
welly50704 <69494769+welly50704@users.noreply.github.com>
2020-12-15 21:23:53 +0800

    Update local_rekognizition.py
    1.The image file'location parameter changed.
    2.update emotions and emotions_count.
    3.detect_face_eqp and detect_head_eqp will also return list,but still have bug.

--- local_rekognizition.py
+++ local_rekognizition.py
@@ -3,22 +3,31 @@
 # 讀取本機檔案識別
 # 檔名預設從1開始
 # 辨識臉部
-def rekog(client, count = 1):
+def rekog(client, imgfilename):
 
-    imgfilename = 'E:/Cloud/OneDrive/課/人工智慧/pic/'+str(count)+'.jpg'
     with open(imgfilename, 'rb') as imgfile:
         imgbytes = imgfile.read()
     rekresp = client.detect_faces(Image={'Bytes': imgbytes}, Attributes=['ALL'])
     rekfd = rekresp['FaceDetails'][0]
 
+#辨識情緒並回傳情緒
+def emotions(client, imgfilename):
+    
+    with open(imgfilename, 'rb') as imgfile:
+        imgbytes = imgfile.read()
+    rekresp = client.detect_faces(Image={'Bytes': imgbytes}, Attributes=['ALL'])
+    rekfd = rekresp['FaceDetails'][0]
+    
+    emotion = rekfd['Emotions'][0]['Type']
+    
+    return emotion
 
 # 讀取本機檔案識別
 # 檔名預設從1開始
 # 如要計數，需要建立dict用以計數
 # 辨識情緒並計數
-def emotions(client, count = 1):
+def emotions_count(client, imgfilename):
     
-    imgfilename = 'E:/Cloud/OneDrive/課/人工智慧/pic/'+str(count)+'.jpg'
     with open(imgfilename, 'rb') as imgfile:
         imgbytes = imgfile.read()
     rekresp = client.detect_faces(Image={'Bytes': imgbytes}, Attributes=['ALL'])
@@ -33,14 +42,13 @@ def emotions(client, count = 1):
     emotion_dict[emotion] += 1                                                  
     print(emotion_dict)
     # 取最多單位的情緒
-    most_emotion = max(emotion_dict, key = emotion_dict.get)                    
-    print(f'目前最多的情緒是{most_emotion}')
-    return most_emotion
+    most_emotions = max(emotion_dict, key = emotion_dict.get)                    
+    print(f'目前最多的情緒是{most_emotions}')
+    return most_emotions
 
 # This function will return bolling, if eyes are close, it return 'True'
-def eyes_close(client, count = 1):
+def eyes_close(client, imgfilename):
 
-    imgfilename = 'E:/Cloud/OneDrive/課/人工智慧/pic/' + str(count) +'.jpg'
     with open(imgfilename, 'rb') as imgfile:
         imgbytes = imgfile.read()
     rekresp = client.detect_faces(Image={'Bytes': imgbytes}, Attributes=['ALL'])
@@ -49,9 +57,8 @@ def eyes_close(client, count = 1):
     return not eyes_close
 
 # This founction will return the number of people
-def count_face(client, count=1):  
+def count_face(client, imgfilename):  
 
-    imgfilename = 'E:/Cloud/OneDrive/課/人工智慧/pic/' + str(count) + '.jpg'
     with open(imgfilename, 'rb') as imgfile:
         imgbytes = imgfile.read()
     rekresp = client.detect_faces(Image={'Bytes': imgbytes}, Attributes=['ALL'])
@@ -59,33 +66,54 @@ def count_face(client, count=1):
     return (len(rekfd))
 
 # if have equipment on face, it will return true.
-def detect_face_eqp(client, count=1):
+def detect_face_eqp(client, imgfilename):
+    no_eqp_list = []
 
-    imgfilename = 'E:/Cloud/OneDrive/課/人工智慧/pic/'+str(count)+'.jpg'
     with open(imgfilename, 'rb') as imgfile:
         imgbytes = imgfile.read()
     rekresp = client.detect_protective_equipment(Image={'Bytes': imgbytes}, 
         SummarizationAttributes={'MinConfidence':80, 'RequiredEquipmentTypes':['FACE_COVER', 'HAND_COVER', 'HEAD_COVER']})
-        
-    '''
-    rekresp['Persons'][0]['BodyParts'][indices]["EquipmentDetections"]
 
-    different indices can check different position' equipment. ###在全部部位都有被偵測到時才成立，因此會做大改########
-    indices = 0 : FACE
-    indices = 1 :LEFT_HAND
-    indices = 2 :RIGHT_HANDx
-    indices = 3 :HEAD
+    persons = rekresp['Persons']
+    for person in persons:
+        Id = person['Id']
+        print(Id)
 
-    '''
-    dt_face_eqp = rekresp['Persons'][0]['BodyParts'][0]["EquipmentDetections"]
- 
-    return dt_face_eqp
+        body_parts = person['BodyParts']
+
+        if len(body_parts) == 0:
+            print('\t\tNo body_part detected')
+            
+        else:
+            for body_part in body_parts:
+                # check have detected head.
+                if body_part['Name'] == 'FACE':  
+                    ppe_items = body_part['EquipmentDetections']
+                    
+                    if not ppe_items:
+                        
+                        no_eqp_list.append(Id)
+                        print(f'{Id}, not have face_epq')
+                        
+
+                    for ppe_item in ppe_items:
+                        head_eqp = ppe_item['CoversBodyPart']['Value']
+                        
+                        if head_eqp == 'False':
+                            no_eqp_list.append(Id)
+                            print(f'{Id}, not have face_epq')
+                           
+    if len(no_eqp_list) != 0:
+            
+        return False, no_eqp_list
+    else:
+        return True, no_eqp_list
 
 # if have equipment on head, it will return true.
 # if someone doesn't have equipment on head, it will print his Id and return false.
-def detect_head_eqp(client, count=1):
+def detect_head_eqp(client, imgfilename):
     no_eqp_list = []
-    imgfilename = 'E:/Cloud/OneDrive/課/人工智慧/pic/'+str(count)+'.jpg'
+
     with open(imgfilename, 'rb') as imgfile:
         imgbytes = imgfile.read()
     rekresp = client.detect_protective_equipment(Image={'Bytes': imgbytes}, 
@@ -121,9 +149,8 @@ def detect_head_eqp(client, count=1):
                             print(f'{Id}, not have head_epq')
                            
     if len(no_eqp_list) != 0:
-        print('以下這些Id可能沒戴安全帽:')
-        for number in no_eqp_list:
-            print(f'Id:{number} ')
-        return False
+            
+        return False, no_eqp_list
     else:
-        return True
+        return True, no_eqp_list
+
